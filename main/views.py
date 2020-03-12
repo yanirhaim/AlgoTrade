7d832dcfc84ae97d245ce148ef21a3d82563b06c
Yanir Haim <yanirhaim@hotmail.com>
2020-03-12 01:01:04 -0400

    No changes, just trying pycharm

--- main/views.py
+++ main/views.py
@@ -3,32 +3,39 @@ from .models import Stock
 from .forms import StockForm
 from django.contrib import messages
 
-def stock (request, stock_id):
+
+def stock(request, stock_id):
     import requests
     import json
 
-    stock = requests.get('https://fmpcloud.io/api/v3/company/profile/'+ stock_id +'?apikey=4f2b01132ec60b46eaa5a5916775d383')
+    stock_data = requests.get(
+        'https://fmpcloud.io/api/v3/company/profile/' + stock_id + '?apikey=4f2b01132ec60b46eaa5a5916775d383')
 
     try:
-        stock = json.loads(stock.content)
+        stock_data = json.loads(stock_data.content)
 
     except Exception as e:
         api = 'Error'
 
     return render(request, "stock.html", {
-        'stock':stock,
+        'stock': stock_data,
     })
 
-#MAIN PAGE
+
+# MAIN PAGE
 def index(request):
     import requests
     import json
 
-    api_gainer = requests.get("https://financialmodelingprep.com/api/v3/stock/gainers?apikey=4f2b01132ec60b46eaa5a5916775d383")
-    api_loser = requests.get("https://financialmodelingprep.com/api/v3/stock/losers?apikey=4f2b01132ec60b46eaa5a5916775d383")
-    api_active = requests.get("https://financialmodelingprep.com/api/v3/stock/actives?apikey=4f2b01132ec60b46eaa5a5916775d383")
-    api_sector = requests.get("https://financialmodelingprep.com/api/v3/stock/sectors-performance?apikey=4f2b01132ec60b46eaa5a5916775d383")
-        
+    api_gainer = requests.get(
+        "https://financialmodelingprep.com/api/v3/stock/gainers?apikey=4f2b01132ec60b46eaa5a5916775d383")
+    api_loser = requests.get(
+        "https://financialmodelingprep.com/api/v3/stock/losers?apikey=4f2b01132ec60b46eaa5a5916775d383")
+    api_active = requests.get(
+        "https://financialmodelingprep.com/api/v3/stock/actives?apikey=4f2b01132ec60b46eaa5a5916775d383")
+    api_sector = requests.get(
+        "https://financialmodelingprep.com/api/v3/stock/sectors-performance?apikey=4f2b01132ec60b46eaa5a5916775d383")
+
     try:
         gainer = json.loads(api_gainer.content)
         loser = json.loads(api_loser.content)
@@ -36,14 +43,14 @@ def index(request):
         sector = json.loads(api_sector.content)
     except Exception as e:
         api = 'Error'
-    
+
     gainer = gainer['mostGainerStock']
     loser = loser['mostLoserStock']
     active = active['mostActiveStock']
     sector = sector['sectorPerformance']
 
     sector_n = []
-    count = 0 
+    count = 0
 
     for items in sector:
         if count < 10:
@@ -52,35 +59,37 @@ def index(request):
         else:
             break
 
-    return render(request,"index.html",{
-        'api':'',
+    return render(request, "index.html", {
+        'api': '',
         'gainer_list': gainer,
-        'loser_list':loser,
-        'active_list':active,
-        'sector_list':sector_n,
+        'loser_list': loser,
+        'active_list': active,
+        'sector_list': sector_n,
     })
 
-#ABOUT PAGE
+
+# ABOUT PAGE
 def about(request):
     return render(request, "about.html", {})
 
-#PORTOFOLIO PAGE
+
+# PORTFOLIO PAGE
 def my_stocks(request):
     import requests
     import json
 
-    #---------------Yahoo API Test-------------------------
+    # ---------------Yahoo API Test-------------------------
     import datetime as dt
     import pandas as pd
     import pandas_datareader.data as web
-    start = dt.datetime(2019,1,1)
+    start = dt.datetime(2019, 1, 1)
     end = dt.datetime.now()
 
     df = web.DataReader('TSLA', 'yahoo', start, end)
     df = df['Close'][-1]
-    #------------------------------------------------------
+    # ------------------------------------------------------
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = StockForm(request.POST or None)
 
         if form.is_valid():
@@ -90,9 +99,10 @@ def my_stocks(request):
 
     else:
         ticker = Stock.objects.all()
-        output=[]
+        output = []
         for ticker_item in ticker:
-            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_b613df2d4a924702ae1e2a134c2bbaba")
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(
+                ticker_item) + "/quote?token=pk_b613df2d4a924702ae1e2a134c2bbaba")
             try:
                 api = json.loads(api_request.content)
                 output.append(api)
@@ -100,17 +110,19 @@ def my_stocks(request):
                 api = 'Error'
 
         return render(request, "my_stocks.html", {
-            'ticker':ticker,
-            'output':output,
+            'ticker': ticker,
+            'output': output,
             'df': df,
         })
 
-#DELETE FUNCTION
+
+# DELETE FUNCTION
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ('You deleted the Stock from your Portfolio'))
     return redirect('my_stocks')
+
 
 def stock_s(request):
     query = request.GET.get('q')
@@ -120,22 +132,24 @@ def stock_s(request):
     import requests
     import json
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + stock_id + "/quote?token=pk_b613df2d4a924702ae1e2a134c2bbaba")
-    r = requests.get('https://finnhub.io/api/v1/stock/profile?symbol='+ stock_id +'&token=bpj80ufrh5rbrf4nci1g')
+    api_request = requests.get(
+        "https://cloud.iexapis.com/stable/stock/" + stock_id + "/quote?token=pk_b613df2d4a924702ae1e2a134c2bbaba")
+    r = requests.get('https://finnhub.io/api/v1/stock/profile?symbol=' + stock_id + '&token=bpj80ufrh5rbrf4nci1g')
 
     try:
         api = json.loads(api_request.content)
         stock_name = api['companyName'].split(',')[0]
         stock_name = stock_name.split()[0]
-        news_request = requests.get("http://newsapi.org/v2/top-headlines?q=" + stock_name + "&apiKey=166d39cbdc1e4442b00b48ec3880f9d6")
+        news_request = requests.get(
+            "http://newsapi.org/v2/top-headlines?q=" + stock_name + "&apiKey=166d39cbdc1e4442b00b48ec3880f9d6")
         news = json.loads(news_request.content)
-        info = json.loads(r.content)        
+        info = json.loads(r.content)
     except Exception as e:
         api = 'Error'
 
     news = news['articles']
 
-    articles = [] 
+    articles = []
     amount_art = 4;
     count = 0;
 
@@ -144,10 +158,8 @@ def stock_s(request):
             articles.append(items)
             count = count + 1
 
-
     return render(request, "stock.html", {
-        'api':api,
-        'news':articles,
-        'info':info,
+        'api': api,
+        'news': articles,
+        'info': info,
     })
-
